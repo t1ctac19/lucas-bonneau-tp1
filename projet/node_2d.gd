b5782e5b5977c285ad79de2a9bbca05a62d7a324
t1ctac19 <2497735@cmontmorency.qc.ca>
2025-09-10 16:07:27 -0400

    dernier d'essaie

--- projet/node_2d.gd
+++ projet/node_2d.gd
@@ -14,36 +14,66 @@ extends Node2D
 @onready var AnimationJaune = $Animation_jaune
 @onready var AnimationBrun = $Animation_brun
 
-var audio_unlocked := false
+
+func _ready():
+
+	SonMonstreBleu.finished.connect(_on_son_bleu_finished)
+	SonMonstreRouge.finished.connect(_on_son_rouge_finished)
+	SonMonstreVert.finished.connect(_on_son_vert_finished)
+	SonMonstreBlanc.finished.connect(_on_son_blanc_finished)
+	SonMonstreJaune.finished.connect(_on_son_jaune_finished)
+	SonMonstreBrun.finished.connect(_on_son_brun_finished)
+
+
 
 func _input(event):
-	if not audio_unlocked and event.is_pressed():
-		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 0)
-		audio_unlocked = true
-		print("Audio HTML5 débloqué !")
+
 
 	if Input.is_action_just_pressed("jouer_son_bleu"):
+		print("Action détectée !")
 		SonMonstreBleu.play()
 		AnimationBleu.play("dance")
+
 	if Input.is_action_just_pressed("jouer_son_rouge"):
+		print("Action détectée !")
 		SonMonstreRouge.play()
 		AnimationRouge.play("dance")
+
 	if Input.is_action_just_pressed("jouer_son_vert"):
+		print("Action détectée !")
 		SonMonstreVert.play()
 		AnimationVert.play("dance")
+
 	if Input.is_action_just_pressed("jouer_son_blanc"):
+		print("Action détectée !")
 		SonMonstreBlanc.play()
 		AnimationBlanc.play("dance")
+
 	if Input.is_action_just_pressed("jouer_son_jaune"):
+		print("Action détectée !")
 		SonMonstreJaune.play()
 		AnimationJaune.play("dance")
+
 	if Input.is_action_just_pressed("jouer_son_brun"):
+		print("Action détectée !")
 		SonMonstreBrun.play()
 		AnimationBrun.play("dance")
 
-func _on_son_bleu_finished(): AnimationBleu.stop()
-func _on_son_rouge_finished(): AnimationRouge.stop()
-func _on_son_vert_finished(): AnimationVert.stop()
-func _on_son_blanc_finished(): AnimationBlanc.stop()
-func _on_son_jaune_finished(): AnimationJaune.stop()
-func _on_son_brun_finished(): AnimationBrun.stop()
+
+func _on_son_bleu_finished():
+	AnimationBleu.stop()
+
+func _on_son_rouge_finished():
+	AnimationRouge.stop()
+
+func _on_son_vert_finished():
+	AnimationVert.stop()
+
+func _on_son_blanc_finished():
+	AnimationBlanc.stop()
+
+func _on_son_jaune_finished():
+	AnimationJaune.stop()
+
+func _on_son_brun_finished():
+	AnimationBrun.stop()
